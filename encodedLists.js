257baa891059d0d6992698395179a50bbe4bbf5a
nicolas.jimenez <nicolasjimenez1337@gmail.com>
2025-12-29 18:56:48 -0300

    updated variable word name, from word to noun

--- encodedLists.js
+++ encodedLists.js
@@ -1,9 +1,7 @@
 // wordsList plain example = ["one", "doppelganger", "roberto", "nebula"];
-const wordDataBase= 'b25lLGRvcHBlbGdhbmdlcixyb2JlcnRvLG5lYnVsYQ==';
-
-export { wordDataBase };
+const nounsDataBase= 'b25lLGRvcHBlbGdhbmdlcixyb2JlcnRvLG5lYnVsYQ==';
+export { nounsDataBase };
 
 // adjectivesList plain example = ["blurry", "beautiful", "raging", "omnipresent"];
 const adjectivesDataBase= 'Ymx1cnJ5LGJlYXV0aWZ1bCxyYWdpbmcsb21uaXByZXNlbnQ=';
-
-export { adjectivesDataBase};
+export { adjectivesDataBase };
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//Passwords should be from 5 to 30 characters long
+//Passwords should be from at least 5 to 30 characters long
 //may include Numbers, Symbol, Capital letters/upper/lower cases
 
-import { wordDataBase, adjectivesDataBase } from "./encodedLists.js";
+import { nounsDataBase, adjectivesDataBase } from "./encodedLists.js";
 
-const words = atob(wordDataBase).split(",");
-console.log(words);
+const nouns = atob(nounsDataBase).split(",");
+console.log(nouns);
 
 const adjectives = atob(adjectivesDataBase).split(",");
 console.log(adjectives);
@@ -19,7 +19,7 @@ let capitalRequirement = true;
 // Generates a random integer from 0 to 50000 inclusive without modulus bias
 function random0torange_unbiased() {
     // const range = 50001; // added 1 because count starts from 0 (0 to 50000 inclusive)
-    const range = words.length; // added 1 because count starts from 0
+    const range = nouns.length; // added 1 because count starts from 0
     const uintMaxPlusOne = 2**32; // Largest int = 2^32
     const limit = Math.floor(uintMaxPlusOne / range) * range; // largest multiple <= 2^32
     const a = new Uint32Array(1);
@@ -31,7 +31,18 @@ function random0torange_unbiased() {
 
 
 // Providing a random word to test the unbiased function
-let place = random0torange_unbiased();
-console.log(place);
-console.log(words[place]);
+// let place = random0torange_unbiased();
+// console.log(place);
+// console.log(words[place]);
 
+// Creating a password
+let adjective = adjectives[random0torange_unbiased()];
+let i=0;
+while (adjective.length > i) {
+    
+    i++;
+}
+let noun = nouns[random0torange_unbiased()];
+
+password = adjective + noun;
+console.log("Enjoy your new password: \n" + password);
