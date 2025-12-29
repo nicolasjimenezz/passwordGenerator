c45d13012e2e7db0c8556eb09b3d256c53dfb27e
nicolas.jimenez <nicolasjimenez1337@gmail.com>
2025-12-29 17:59:28 -0300

    added new file containing the beta words database

---
+++ encodedLists.js
@@ -0,0 +1,9 @@
+// wordsList plain example = ["one", "doppelganger", "roberto", "nebula"];
+const wordDataBase= 'b25lLGRvcHBlbGdhbmdlcixyb2JlcnRvLG5lYnVsYQ==';
+
+export { wordDataBase };
+
+// adjectivesList plain example = ["blurry", "beautiful", "raging", "omnipresent"];
+const adjectivesDataBase= 'Ymx1cnJ5LGJlYXV0aWZ1bCxyYWdpbmcsb21uaXByZXNlbnQ=';
+
+export { adjectivesDataBase};
--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 //Passwords should be from 5 to 30 characters long
-//Should include Numbers, Symbol, Capital letters/upper/lower cases
-//Allowed symbol
+//may include Numbers, Symbol, Capital letters/upper/lower cases
 
-let words = ["one", "Doppelganger", "Roberto"];
+import { wordDataBase, adjectivesDataBase } from "./encodedLists.js";
+
+const words = atob(wordDataBase).split(",");
+console.log(words);
+
+const adjectives = atob(adjectivesDataBase).split(",");
+console.log(adjectives);
+
 
 let password;
-
-// let array = new Uint16Array(1);
-// crypto.getRandomValues(array);
-// console.log(array[0]);
-// console.log(array);
 
 let symbolRequirement = true;
 let numberRequirement = true;
 let capitalRequirement = true;
 
-
-
-let array=[];
-for (let i = 0; i < 100; i++) {
-    array.push(random0to50000_unbiased());
-}
-console.log(array);
-
 // Generates a random integer from 0 to 50000 inclusive without modulus bias
-function random0to50000_unbiased() {
-    const range = 50001; // number of values (0..50000)
-    const uintMaxPlusOne = 0x100000000; // 2^32
+function random0torange_unbiased() {
+    // const range = 50001; // added 1 because count starts from 0 (0 to 50000 inclusive)
+    const range = words.length; // added 1 because count starts from 0
+    const uintMaxPlusOne = 2**32; // Largest int = 2^32
     const limit = Math.floor(uintMaxPlusOne / range) * range; // largest multiple <= 2^32
     const a = new Uint32Array(1);
     while (true) {
@@ -34,4 +28,10 @@ function random0to50000_unbiased() {
         if (a[0] < limit) return a[0] % range;
     }
 }
-console.log(random0to50000_unbiased())
+
+
+// Providing a random word to test the unbiased function
+let place = random0torange_unbiased();
+console.log(place);
+console.log(words[place]);
+
